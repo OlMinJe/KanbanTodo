3e1b01d96e00e79eea5911769219a74a61df24b4
OlMinJe <Ol.MinJe@gmail.com>
2025-08-21 15:36:07 +0900

    💄 Design: 메인의 기본 레이아웃 컴포넌트 UI 작업 진행 완료 #6

--- src/routes/layout/DefaultLayout.tsx
+++ src/routes/layout/DefaultLayout.tsx
@@ -1,13 +1,15 @@
 import Footer from '@/components/Footer'
-import Header from '@/components/Header'
+import Header from '@/components/header/Header'
 import { Outlet } from 'react-router'
 
 export default function DefaultLayout() {
   return (
-    <>
+    <div className="w-dvw min-h-screen min-h-dvh flex flex-col">
       <Header />
-      <Outlet />
+      <main className="p-5 flex-1 min-h-0 overflow-auto">
+        <Outlet />
+      </main>
       <Footer />
-    </>
+    </div>
   )
 }

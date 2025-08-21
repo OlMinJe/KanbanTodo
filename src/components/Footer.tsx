7cac3c289f2d99189e929d0d9e6ca9c7e77114de
OlMinJe <Ol.MinJe@gmail.com>
2025-08-21 15:19:14 +0900

    💄Design: 푸터 컴포넌트 반응형 작업 진행 #3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,37 @@
+import { NavLink } from 'react-router'
+
 export default function Footer() {
   return (
-    <div>
-      <h1>푸터 영역</h1>
-    </div>
+    <footer className="p-5 bg-orange-100 text-center">
+      <div className="text-gray-400">&copy; 2025 MINJE LEE. All rights reserved.</div>
+      <ul className="flex justify-center gap-3">
+        <li>
+          <label htmlFor="emailModalToggle" className="modal-open-button">
+            <i className="fa-solid fa-envelope" />
+          </label>
+          <input type="checkbox" id="emailModalToggle" hidden />
+        </li>
+        <li>
+          <NavLink
+            to="https://velog.io/@ol_minje/posts"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="블로그"
+          >
+            <i className="fa-solid fa-pen" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="https://github.com/OlMinJe"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <i className="fa-brands fa-github" />
+          </NavLink>
+        </li>
+      </ul>
+    </footer>
   )
 }

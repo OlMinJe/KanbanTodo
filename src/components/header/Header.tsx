1bc4270f69a8bb747602c7eb4f4221cc631cf4f8
OlMinJe <Ol.MinJe@gmail.com>
2025-08-21 15:14:32 +0900

    💄Design: 헤더 컴포넌트 반응형 작업 진행 #2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,16 +2,17 @@ import MainNav from '@/components/header/MainNav'
 import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
 import { Moon, Sun } from 'lucide-react'
+import { NavLink } from 'react-router'
 
 export default function Header() {
   return (
     <header>
       <h1 className="sr-only">헤더 영역입니다.</h1>
       <div className="px-5 py-5 w-full flex justify-between justify-items-center">
-        <div className="flex items-center">
+        <NavLink to="/" className="flex items-center">
           <img src="/logo.svg" alt="logo image" className="w-10 mr-1" />
-          칸투펀치
-        </div>
+          <span className="hidden md:inline">칸투펀치</span>
+        </NavLink>
         <MainNav />
         <div className="flex items-center space-x-2">
           <Label htmlFor="theme-mode">

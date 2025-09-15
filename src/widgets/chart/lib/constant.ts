1d63f03f01c970437a38f0f09ee0d690a5fdfcb3
OlMinJe <Ol.MinJe@gmail.com>
2025-09-15 16:48:42 +0900

    ✨ Feat: 차트에 사용될 config 정의 #29

---
+++ src/widgets/chart/lib/constant.ts
@@ -0,0 +1,69 @@
+import { TODO_STATUS, type STATUS_TYPE } from '@/entities/todo'
+import type { ChartConfig } from '@/shared/ui/shadcn'
+
+export const STATUS_ORDER: STATUS_TYPE[] = [
+  TODO_STATUS.TODO,
+  TODO_STATUS.DOING,
+  TODO_STATUS.DEFER,
+  TODO_STATUS.DONE,
+  TODO_STATUS.REMOVE,
+]
+
+export const barChartConfig = {
+  count: {
+    label: '작업 갯수',
+    color: 'var(--chart-1)',
+  },
+} satisfies ChartConfig
+
+export const barCharHorizontalConfig = {
+  status: {
+    label: '작업 상태',
+  },
+  todo: {
+    label: 'todo',
+    color: 'var(--chart-1)',
+  },
+  doing: {
+    label: 'doing',
+    color: 'var(--chart-2)',
+  },
+  defer: {
+    label: 'defer',
+    color: 'var(--chart-3)',
+  },
+  done: {
+    label: 'done',
+    color: 'var(--chart-4)',
+  },
+  remove: {
+    label: 'remove',
+    color: 'var(--chart-5)',
+  },
+} satisfies ChartConfig
+
+export const pieChartConfig = {
+  count: {
+    label: '작업 갯수',
+  },
+  todo: {
+    label: 'todo',
+    color: 'var(--chart-1)',
+  },
+  doing: {
+    label: 'doing',
+    color: 'var(--chart-2)',
+  },
+  defer: {
+    label: 'defer',
+    color: 'var(--chart-3)',
+  },
+  done: {
+    label: 'done',
+    color: 'var(--chart-4)',
+  },
+  remove: {
+    label: 'remove',
+    color: 'var(--chart-5)',
+  },
+} satisfies ChartConfig

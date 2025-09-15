02245cede1d75dad9116ed173fe3882be058a5f2
OlMinJe <Ol.MinJe@gmail.com>
2025-09-15 16:47:40 +0900

    🔧 Chore: 통계 페이지에 차트 컴포넌트 연결 #29

--- src/pages/statsChart/index.tsx
+++ src/pages/statsChart/index.tsx
@@ -1,3 +1,14 @@
+import { AreaChart, BarCharHorizontal, BarChart, PieChartCountText } from '@/widgets/chart'
+
 export default function StatsChart() {
-  return <h1>StatsChart</h1>
+  return (
+    <div className="min-w-[320px] max-w-3xl mx-auto my-0">
+      <div className="mb-4 grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+        <BarChart />
+        <BarCharHorizontal />
+        <PieChartCountText />
+      </div>
+      <AreaChart />
+    </div>
+  )
 }

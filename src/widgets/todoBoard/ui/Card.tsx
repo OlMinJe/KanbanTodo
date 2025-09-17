537999f73be357ac2603996849009936b8444564
OlMinJe <Ol.MinJe@gmail.com>
2025-09-17 13:53:42 +0900

    💄 Style: 할 일 카드 동일하게 규정 #35

--- src/widgets/todoBoard/ui/Card.tsx
+++ src/widgets/todoBoard/ui/Card.tsx
@@ -10,9 +10,14 @@ type Props = {
 
 export default function Card({ todo, renderMenu }: Props) {
   return (
-    <Shadcn.Card className="flex shadow-md rounded-xl p-5">
+    <Shadcn.Card className="flex rounded-xl p-3">
       <Shadcn.CardHeader className="flex flex-row items-center justify-between p-0">
-        <Shadcn.CardTitle className="text-sm font-semibold">{todo.title}</Shadcn.CardTitle>
+        <Shadcn.CardTitle
+          className={['pr-2 text-sm font-semibold tracking-tight line-clamp-1 '].join(' ')}
+          title={todo.title}
+        >
+          {todo.title}
+        </Shadcn.CardTitle>
         {renderMenu ?? (
           <button type="button" className="opacity-60">
             <Ellipsis width={18} height={18} />
@@ -21,10 +26,19 @@ export default function Card({ todo, renderMenu }: Props) {
       </Shadcn.CardHeader>
 
       <Shadcn.CardContent className="text-sm flex-1 flex flex-col gap-1 p-0">
-        {todo.description && <p className="text-xs text-gray-700 mb-2">{todo.description}</p>}
+        {todo.description ? (
+          <p
+            className="line-clamp-1 text-xs text-zinc-600 dark:text-zinc-400"
+            title={todo.description}
+          >
+            {todo.description}
+          </p>
+        ) : (
+          <div className="h-[12px]" />
+        )}
 
         <div className="flex justify-between items-center">
-          {todo.tags?.length ? (
+          {/* {todo.tags?.length ? (
             <div className="flex flex-wrap gap-1">
               {todo.tags.map((tag) => (
                 <span
@@ -37,7 +51,12 @@ export default function Card({ todo, renderMenu }: Props) {
             </div>
           ) : (
             <span />
-          )}
+          )} */}
+          <div className="flex flex-wrap gap-1">
+            <span className="text-[10px] px-2 py-0.5 rounded-full bg-gray-100 text-gray-600">
+              태그 - 추후
+            </span>
+          </div>
 
           <span className="text-xs text-gray-500">{todo.priority}</span>
         </div>

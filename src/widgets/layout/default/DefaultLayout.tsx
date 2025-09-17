03e4fb8614718407bae1ec887442d923d20ac4b2
OlMinJe <Ol.MinJe@gmail.com>
2025-09-17 15:24:12 +0900

    💄 Style: 전체 레이아웃 너비 뷰포트 기준에서 콘텐츠 기준 full로 수정 #36

--- src/widgets/layout/default/DefaultLayout.tsx
+++ src/widgets/layout/default/DefaultLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router'
 
 export default function DefaultLayout() {
   return (
-    <div className="w-dvw min-h-screen min-h-dvh flex flex-col">
+    <div className="w-full min-h-screen min-h-dvh flex flex-col">
       <Header />
-      <main className="p-5 flex-1 min-h-0 overflow-auto">
+      <main className="p-5 flex-1 min-h-0overflow-hidden">
         <Outlet />
       </main>
       <Footer />
